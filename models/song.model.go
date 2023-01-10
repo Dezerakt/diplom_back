4e8ee5b3e52b412822a74fbae720988d26c4f03f
AIsaev <isaev3658@gmail.com>
2023-01-10 15:31:20 +0600

    small changes for song model, and few records for seeder

--- models/song.model.go
+++ models/song.model.go
@@ -5,7 +5,6 @@ import "gorm.io/gorm"
 type Song struct {
 	gorm.Model
 	AlbumID       uint   `json:"album_id"`
-	Duration      string `json:"duration"`
 	Name          string `json:"name"`
 	SpotifyWidget string `json:"spotify_widget"`
 }
